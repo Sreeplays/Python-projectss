969b046b6c1ff2e19e9acbbefdd8da57673a388f
KalaOfKerala <sriram.7b17@gmail.com>
2023-03-13 23:40:31 +0530

    change in the background of the application

--- Python-tkinter-projects/Screenrec.py
+++ Python-tkinter-projects/Screenrec.py
@@ -3,20 +3,22 @@ import pyscreenrec
 
 #Configuration of the window
 root = Tk()
-root.geometry("400x600")
+root.geometry("800x600")
 root.title("PlaysRec")
 root.resizable(True, True)
-root.config(bg="#fff")
+root.config(bg="#2F2F4F")
 
 #Starting of the rec configuration 
 rec = pyscreenrec.ScreenRecorder()
 
 #icons
-image_icon = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Images\stop-button.png")
+image_icon = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Images\screen-rec.png")
 root.iconphoto(False, image_icon)
 
 #Background 
 
 image1 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Images\des2.png")
-Label(root, image=image1, bg="#fff").place(x=0, y=0)
+Label(root, image=image1, bg="#2F2F4F").place(x=350, y=30)
+
+
 root.mainloop()
