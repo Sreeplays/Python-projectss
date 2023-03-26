7fd70da37c68f8508e974466a2c1c0a15392a7fa
KalaOfKerala <sriram.7b17@gmail.com>
2023-03-26 13:32:29 +0530

    Final commit(probably)

--- Screenrec.py
+++ Screenrec.py
@@ -27,14 +27,14 @@ def resume_rec():
 rec = pyscreenrec.ScreenRecorder()
 
 #icons
-image_icon = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Images\screen-rec.png")
+image_icon = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Dist\Images\screen-rec.png")
 root.iconphoto(False, image_icon)
 
 #Background 
 
-image1 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Images\yelllow1.png")
+image1 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Dist\Images\yelllow1.png")
 Label(root, image=image1, bg="#969696").place(x=-2, y=20)
-image =  PhotoImage(file=r"D:\Sriram\Python projects\Python-tkinter-projects\Images\blue.png")
+image =  PhotoImage(file=r"D:\Sriram\Python projects\Python-tkinter-projects\Dist\Images\blue.png")
 Label(root, image=image, bg="#969696").place(x=635, y=400)
 #Heading 
 heading = Label(root, text="PlayzRec", bg="#969696", font="arial 20 bold",)
@@ -44,7 +44,7 @@ heading1 = Label(root, text="(Click on the 'record' button)", bg="#969696", font
 heading1.pack(pady=50)
 heading1.place(x=303, y=75)
 #Heading image
-image3 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Images\screen-rec.png")
+image3 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Dist\Images\screen-rec.png")
 Label(root, image=image3, bg="#969696").pack(pady=100)
 
 #entry
@@ -60,21 +60,21 @@ filenametext.place(x=300, y=345)
 start =  Button(root, text="Rec", bg="#fff", font="arial 15 bold", bd=0, command=start_rec)
 start.place(x=367, y=161)
 #PAUSE
-image4 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Images\pause.png")
+image4 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Dist\Images\pause.png")
 pause = Button(root, image=image4, bg="#969696", bd=0, command=pause_rec)
 pause.place(x=80, y=450)
 pausetext = Label(root, text="Pause", font="Helvetica 10 bold", bd=0, bg="#969696")
 pausetext.pack(pady=20)
 pausetext.place(x=95, y=530)
 #RESUME
-image5 = PhotoImage(file=r"D:\Sriram\Python projects\Python-tkinter-projects\Images\unpause.png")
+image5 = PhotoImage(file=r"D:\Sriram\Python projects\Python-tkinter-projects\Dist\Images\unpause.png")
 resume = Button(root, image=image5, bg="#969696", bd=0, command=resume_rec)
 resume.place(x=350, y=450)
 resumetext = Label(root, text="Pause", font="Helvetica 10 bold", bd=0, bg="#969696")
 resumetext.pack(pady=20)
 resumetext.place(x=365, y=530)
 #STOP
-image6 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Images\stop.png")
+image6 = PhotoImage(file="D:\Sriram\Python projects\Python-tkinter-projects\Dist\Images\stop.png")
 stop = Button(root, image=image6, bg="#969696", bd=0, command=stop_rec)
 stop.place(x=585, y=450)
 stoptext = Label(root, text="Stop", font="Helvetica 10 bold", bd=0, bg="#969696")
